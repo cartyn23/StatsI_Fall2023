f9bdc05c1a09950d65f17d03a2f01551be9bbf8b
cartyn23 <144428671+cartyn23@users.noreply.github.com>
2023-09-27 13:34:54 +0100

    Create PS01_myanswers_NC.R

---
+++ problemSets/PS01/my_answers/PS01_myanswers_NC.R
@@ -0,0 +1,258 @@
+#####################
+# load libraries
+# set wd
+# clear global .envir
+#####################
+
+# Get working directory
+getwd()
+
+# Set working directory 
+setwd("/Users/carty/OneDrive/Documents/GitHub/StatsI_Fall2023/problemSets/PS01/my_answers")
+getwd()
+
+# remove objects
+rm(list=ls())
+
+# detach all libraries
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats", "package:graphics", "package:grDevices", "package:utils", "package:datasets", "package:methods", "package:base")
+  package.list <- search()[ifelse(unlist(gregexpr("package:", search()))==1, TRUE, FALSE)]
+  package.list <- setdiff(package.list, basic.packages)
+  if (length(package.list)>0)  for (package in package.list) detach(package,  character.only=TRUE)
+}
+detachAllPackages()
+
+# load libraries
+pkgTest <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[,  "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg,  dependencies = TRUE)
+  sapply(pkg,  require,  character.only = TRUE)
+}
+
+# here is where you load any necessary packages
+# ex: stringr
+# lapply(c("stringr"),  pkgTest)
+
+lapply(c(),  pkgTest)
+
+#####################
+# Problem 1
+#####################
+
+# QUESTION 1: find a 90% Confidence Interval for the average IQ in the school
+# below is a random sample of 25 students' IQ scores
+
+y <- c(105, 69, 86, 100, 82, 111, 104, 110, 87, 108, 87, 90, 94, 113, 112, 98, 80, 97, 95, 111, 114, 89, 95, 126, 98)
+
+# let's look at key data
+
+sort(y) # reorder scores
+mean(y) # Central tendency, mean
+var(y) # Variability, variance
+sd(y) # Variability, standard deviation
+sd(y) / sqrt(length(y))  # Variability, standard **error**
+
+summary(y)
+
+# Some quick visualizations, to look at distribution
+hist(y, 
+     breaks = 5,
+     main = "Students' IQ scores",
+     xlab = "IQ score")
+
+plot(y,
+     main="Students' IQ scores",
+     ylab="IQ scores")
+
+#To find the 90% Confidence Interval, we look for the Point estimate +/- Margin of error, 
+# where margin of error is a multiple of the standard error
+
+# What do we need?
+mean(y) # Central tendency, mean - a point estimate
+sd(y) / sqrt(length(y))  # Variability, the standard error
+
+# How to find the multiple? 
+# Looking at the normal distribution, we see that 90% of observations lie within +/-1.64 
+# standard errors of the point estimate 
+
+?qnorm
+qnorm(0.05) # value for first 5%
+qnorm(0.95) # value last 5%
+
+# Solution method 1: The **approximate** solution for 90% confidence level
+
+upper_90 = mean(y)+(1.64*sd(y) / sqrt(length(y)))
+lower_90 = mean(y)-(1.64*sd(y) / sqrt(length(y)))
+
+#Summary
+lower_90
+mean(y)
+upper_90
+
+# SOlution method 2: The **precise** solution, using normal distribution for 90% confidence level
+
+lower_90_n <- qnorm(0.05, 
+                    mean = mean(y), 
+                    sd = (sd(y) / sqrt(length(y))))
+
+# Upper bound, 95 confidence level
+upper_90_n <- qnorm(0.95,
+                    mean = mean(y),
+                    sd = (sd(y) / sqrt(length(y))))
+
+
+# On this basis, we can conclude that the confidence interval lies between 94.13283 and 102.7472
+
+
+# QUESTION 2: is the average student IQ in the school higher than the average IQ score (100) 
+# in schools across the country?
+
+# As the sample size is less than 30 we conduct a T-test
+
+# It will be a one-sided test as we are looking to see if the student IQ in the school 
+# is higher than the pop average
+
+# The NULL Hypothesis is that there is no difference 
+
+# Alternative Hypothesis is that the IQ is higher in the school
+
+# The significance level is set at 0.05
+
+# First step: calculate the standard error
+
+standard_error <-  sd(y) / sqrt(length(y))  # Variability, standard error
+
+# Find T statistic
+
+t_statistic <- abs((mean(y) - 100) / standard_error)
+
+# Find probability score
+
+prob <- pt(t_statistic, (length(y)-1))
+
+# p_value = 0.7215383
+
+# Therefore we have evidence to accept the null hypothesis, as prob > 0.05
+
+# METHOD 2
+
+# calculate the t_score for a one-sided test, with confidence level of 0.05
+
+t.test(y, mu = 100, conf.level = 0.05, alternative = "greater")
+
+# Update Histogram 
+
+hist(y, 
+     breaks = 5,
+     abline(v=mean(y),col="black"),
+     main = "Students' IQ scores",
+     xlab = "IQ score")
+
+abline(v=mean(y),col="black")
+abline(v=lower_90,col="black",lty="dashed")
+abline(v=upper_90,col="black",lty="dashed")
+
+
+
+#####################
+# Problem 2
+#####################
+
+expenditure <- read.table("https://raw.githubusercontent.com/ASDS-TCD/StatsI_Fall2023/main/datasets/expenditure.txt", header=T)
+
+# QUESTION 2: is the average student IQ in the school higher than the average IQ score (100) 
+# in schools across the country?
+
+# As the sample size is less than 30 we conduct a T-test
+
+# It will be a one-sided test as we are looking to see if the student IQ in the school 
+# is higher than the pop average
+
+# The NULL Hypothesis is that there is no difference 
+
+# Alternative Hypothesis is that the IQ is higher in the school
+
+# The significance level is set at 0.05
+
+# First step: calculate the standard error
+
+sd(y) / sqrt(length(y))  # Variability, standard error
+
+# calculate the t_score for a one-sided test, with confidence level of 0.05
+
+#Right side critical value 95% confidence level, as looking to see if score is higher −
+
+t_score <- abs(qt(0.05, df=(length(y)-1)))
+
+#Test score = 1.710882, df = length((y)-1)
+
+
+#find one-tail p-value with right sided test
+
+P_value <- pt(q = 1.710882, df = (length(y)-1), lower.tail = FALSE)
+
+#The p-value of 0.05000001 is approximately equal to the significance level of 0.05
+
+# for this reason, we found evidence that we can reject the null hypothesis, as with repeated sampling, 
+# we can expect the p-value to fall within the area of significance
+
+
+# Update Histogram 
+
+hist(y, 
+     breaks = 5,
+     abline(v=mean(y),col="black"),
+     main = "Students' IQ scores",
+     xlab = "IQ score")
+
+abline(v=mean(y),col="black")
+abline(v=lower_90,col="black",lty="dashed")
+abline(v=upper_90,col="black",lty="dashed")
+
+
+
+#####################
+# Problem 2
+#####################
+
+expenditure <- read.table("https://raw.githubusercontent.com/ASDS-TCD/StatsI_Fall2023/main/datasets/expenditure.txt", header=T)
+
+#Please plot the relationships among Y, X1, X2, and X3 ? What are the correlations
+# among them (you just need to describe the graph and the relationships among them)?
+
+#Please plot the relationship between Y and Region? On average, which region has the
+#highest per capita expenditure on housing assistance?
+
+#Please plot the relationship between Y and X1 ? Describe this graph and the relationship. #Reproduce the above graph including one more variable Region and display
+#different regions with different types of symbols and colors
+
+plot(expenditure$Y, expenditure$X1)
+plot(expenditure$Y, expenditure$X2)
+plot(expenditure$Y, expenditure$X3)
+plot(expenditure$X1, expenditure$X2)
+plot(expenditure$X1, expenditure$X3)
+plot(expenditure$X2, expenditure$X3)
+
+cor(expenditure$Y, expenditure$X1)
+cor(expenditure$Y, expenditure$X2)
+cor(expenditure$Y, expenditure$X3)
+cor(expenditure$X1, expenditure$X2)
+cor(expenditure$X1, expenditure$X3)
+cor(expenditure$X2, expenditure$X3)
+
+# the strongest associations observed are between per capital expenditure on shelters/housing assistance in the state (Y) 
+# and per capita personal income in state (X1) and also the number of people living in urban areas in the state (X3)
+
+# the weakest associations are between the per capital personal income in state (X1) and number of residents per 100,00 that are 
+# "financially secure" in state (X2), as well as between number of residents that are financially secure (X2)
+#and the number of people residing in urban areas of the state
+
+
+#Please plot the relationship between Y and Region? On average, which region has the
+#highest per capita expenditure on housing assistance?
+
+plot(expenditure$Y, expenditure$Region)
+
+y_region <- lm(expenditure$Y~expenditure$Region, data=expenditure)
