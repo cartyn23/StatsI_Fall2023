576af2d57afe579c0574cacdf6f6ff9522e38f74
cartyn23 <144428671+cartyn23@users.noreply.github.com>
2023-10-01 15:27:34 +0100

    My Answers for Problem Set 01

--- problemSets/PS01/my_answers/PS01_myanswers_NC.R
+++ problemSets/PS01/my_answers/PS01_myanswers_NC.R
@@ -162,85 +162,24 @@ abline(v=upper_90,col="black",lty="dashed")
 
 expenditure <- read.table("https://raw.githubusercontent.com/ASDS-TCD/StatsI_Fall2023/main/datasets/expenditure.txt", header=T)
 
-# QUESTION 2: is the average student IQ in the school higher than the average IQ score (100) 
-# in schools across the country?
-
-# As the sample size is less than 30 we conduct a T-test
-
-# It will be a one-sided test as we are looking to see if the student IQ in the school 
-# is higher than the pop average
-
-# The NULL Hypothesis is that there is no difference 
-
-# Alternative Hypothesis is that the IQ is higher in the school
-
-# The significance level is set at 0.05
-
-# First step: calculate the standard error
-
-sd(y) / sqrt(length(y))  # Variability, standard error
-
-# calculate the t_score for a one-sided test, with confidence level of 0.05
-
-#Right side critical value 95% confidence level, as looking to see if score is higher −
-
-t_score <- abs(qt(0.05, df=(length(y)-1)))
-
-#Test score = 1.710882, df = length((y)-1)
-
-
-#find one-tail p-value with right sided test
-
-P_value <- pt(q = 1.710882, df = (length(y)-1), lower.tail = FALSE)
-
-#The p-value of 0.05000001 is approximately equal to the significance level of 0.05
-
-# for this reason, we found evidence that we can reject the null hypothesis, as with repeated sampling, 
-# we can expect the p-value to fall within the area of significance
-
-
-# Update Histogram 
-
-hist(y, 
-     breaks = 5,
-     abline(v=mean(y),col="black"),
-     main = "Students' IQ scores",
-     xlab = "IQ score")
-
-abline(v=mean(y),col="black")
-abline(v=lower_90,col="black",lty="dashed")
-abline(v=upper_90,col="black",lty="dashed")
-
-
-
-#####################
-# Problem 2
-#####################
-
-expenditure <- read.table("https://raw.githubusercontent.com/ASDS-TCD/StatsI_Fall2023/main/datasets/expenditure.txt", header=T)
-
 #Please plot the relationships among Y, X1, X2, and X3 ? What are the correlations
 # among them (you just need to describe the graph and the relationships among them)?
 
-#Please plot the relationship between Y and Region? On average, which region has the
-#highest per capita expenditure on housing assistance?
+exp_per_capita <- expenditure$Y
+state_income <- expenditure$X1
+fin_secure <- expenditure$X2
+urban_areas <- expenditure$X3
+region <-  expenditure$Region
+factor_region <- factor(region, levels = c("1", "2", "3", "4"))
+levels(factor_region) <- c("Northeast", "North Central", "South", "West")
 
-#Please plot the relationship between Y and X1 ? Describe this graph and the relationship. #Reproduce the above graph including one more variable Region and display
-#different regions with different types of symbols and colors
+install.packages("ggplot2")       
+install.packages("GGally")
+library("ggplot2")                     
+library("GGally")
 
-plot(expenditure$Y, expenditure$X1)
-plot(expenditure$Y, expenditure$X2)
-plot(expenditure$Y, expenditure$X3)
-plot(expenditure$X1, expenditure$X2)
-plot(expenditure$X1, expenditure$X3)
-plot(expenditure$X2, expenditure$X3)
-
-cor(expenditure$Y, expenditure$X1)
-cor(expenditure$Y, expenditure$X2)
-cor(expenditure$Y, expenditure$X3)
-cor(expenditure$X1, expenditure$X2)
-cor(expenditure$X1, expenditure$X3)
-cor(expenditure$X2, expenditure$X3)
+data.frame(expenditure)
+ggpairs(expenditure, columns = 2:5)
 
 # the strongest associations observed are between per capital expenditure on shelters/housing assistance in the state (Y) 
 # and per capita personal income in state (X1) and also the number of people living in urban areas in the state (X3)
@@ -253,6 +192,33 @@ cor(expenditure$X2, expenditure$X3)
 #Please plot the relationship between Y and Region? On average, which region has the
 #highest per capita expenditure on housing assistance?
 
-plot(expenditure$Y, expenditure$Region)
+ggplot(expenditure, aes(x = exp_per_capita, y = factor_region)) +
+  geom_boxplot() +
+  labs(x = "per capita expenditure on shelters/housing assistance in state",
+       y = "regions") 
 
-y_region <- lm(expenditure$Y~expenditure$Region, data=expenditure)
+
+# region 4 has the highest expenditure on housing assistance.
+
+#Please plot the relationship between Y and X1 ? Describe this graph and the relationship. #Reproduce the above graph including one more variable Region and display
+#different regions with different types of symbols and colors
+
+ggplot(expenditure, aes(x = state_income, y = exp_per_capita)) +
+  geom_point(size = 2, color = "red") +
+  labs(x = "per capita personal income in state", y = "per capita expenditure on shelters/housing assistance in state" )
+
+cor(exp_per_capita, state_income)
+
+# with Region added
+
+ggplot(expenditure, aes(x = state_income, y = exp_per_capita, color = factor_region, shape = factor_region)) +
+  geom_point(size = 2) +
+  labs(x = "per capita personal income in state", 
+       y = "per capita expenditure on shelters/housing assistance in state", 
+       color = "Regions", 
+       shape = "Regions")
+
+
+# The correlation is 0.532 and you can see a strong correlation in particular across region 3, and then region 2. 
+
+#Regions 1 and 4 appear to have a less strong correlation.
